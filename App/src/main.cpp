d1d4808aeff5e2762a4056ee31afb4d86bfb4dca
Stehack <82880802+stehack27@users.noreply.github.com>
2022-06-28 23:31:31 +0200

    Update main.cpp

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -12,7 +12,6 @@ DWORD WINAPI MainThread(HMODULE hModule) {
 	}
 }
 
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
