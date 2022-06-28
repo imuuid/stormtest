a40f52311af9003bf0670e5e126088cbc2b3e70f
Stehack <82880802+stehack27@users.noreply.github.com>
2022-06-28 23:17:16 +0200

    Update main.h

--- App/src/main.h
+++ App/src/main.h
@@ -14,12 +14,12 @@ inline const char* html = R"(
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
-    <link href="https://raw.githubusercontent.com/stehack27/storm/main/Web/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/gh/stehack27/storm/Web/style.css" rel="stylesheet">
     <title> </title>
   </head>
   <body id="mainContainer">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://raw.githubusercontent.com/stehack27/storm/main/Web/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/stehack27/storm/Web/main.js"></script>
   </body>
 </html>
 )";
